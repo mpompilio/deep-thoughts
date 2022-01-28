1ac78aae5d60bc3c3892ef27ea243a3734445c5f
Matthew Pompilio <mattflynnpomp@aol.com>
2022-01-28 11:12:59 -0500

    combining server and client

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2022 by Matthew Pompilio</div>
+    </footer>
+  );
+};
+
+export default Footer;
